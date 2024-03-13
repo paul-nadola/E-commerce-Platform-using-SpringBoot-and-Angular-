6e34a47645eb80a63c61ab816cf9ed5cc8085966
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-03-13 12:26:32 +0300

    first commit

---
+++ src/main/java/EcomercePlatformDemoApp/EcomercePlatformApplication.java
@@ -0,0 +1,13 @@
+package EcomercePlatformDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EcomercePlatformDemoApp/EcomercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package EcomercePlatformDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
